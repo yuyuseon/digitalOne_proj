43066421243c21dee741678d87fa071c999052bb
a950626 <a950626@DESKTOP-8D7IMUN>
2019-05-28 16:29:57 +0900

    부트스트랩 템플릿 적용

--- Book/src/main/java/com/DigitalOne/Book/HomeController.java
+++ Book/src/main/java/com/DigitalOne/Book/HomeController.java
@@ -33,7 +33,20 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "main";
 	}
 	
+	@RequestMapping(value = "/home2", method = RequestMethod.GET)
+	public String home2(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home2";
+	}
 }
